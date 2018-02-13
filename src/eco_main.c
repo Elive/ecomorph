6052b7d321fb44a2ddbf5c869ffcc685e7c81a43
Thanatermesis <thanatermesis@gmail.com>
2018-02-13 21:33:23 +0100

    Revert "Revert "load ecomorph at start""
    This reverts commit 4a61296c017dae65473f312fd8dd932ec2ada7a3.

--- src/eco_main.c
+++ src/eco_main.c
@@ -92,7 +92,8 @@ Eina_Bool ecomorph_ready(void *data)
 {
    if(config->ready)
      {
-        e_mod_run_ecomp_sh();
+        /*e_mod_run_ecomp_sh();*/
+        e_mod_run_ecomorph();
         return ECORE_CALLBACK_DONE;
      }
    return ECORE_CALLBACK_RENEW;
