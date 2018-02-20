0c3d884d08616b96cc7ab157d65d3bbd0b4ffb58
Thanatermesis <thanatermesis@gmail.com>
2018-02-20 22:27:54 +0100

    commented unused code to not confuse us

--- src/eco_main.c
+++ src/eco_main.c
@@ -11,7 +11,7 @@ int ecomorph_log;
 static void  _e_mod_run_cb(void *data, E_Menu *m, E_Menu_Item *mi);
 static void  _e_mod_menu_add(void *data, E_Menu *m);
 static Eina_Bool _e_mod_event_del_handler(void *data, int type, void *event);
-void e_mod_run_ecomp_sh();
+/*void e_mod_run_ecomp_sh();*/
 
 Ecore_X_Atom ECOMORPH_ATOM_MANAGED = 0;
 Ecore_X_Atom ECOMORPH_ATOM_MOVE_RESIZE = 0;
@@ -375,131 +375,131 @@ _e_mod_event_del_handler(void *data, int type, void *event)
    return ECORE_CALLBACK_DONE;
 }
 
-static Eina_Bool
-_e_mod_event_data_handler_ecomp_sh(void *data, int type, void *event)
-{
-   Ecore_Exe_Event_Data *eed;
-   char **arr;
-   int i;
+/*static Eina_Bool*/
+/*_e_mod_event_data_handler_ecomp_sh(void *data, int type, void *event)*/
+/*{*/
+   /*Ecore_Exe_Event_Data *eed;*/
+   /*char **arr;*/
+   /*int i;*/
 
-   if(!(eed = event)) return ECORE_CALLBACK_CANCEL;
-   if(!eed->exe) return ECORE_CALLBACK_CANCEL;
+   /*if(!(eed = event)) return ECORE_CALLBACK_CANCEL;*/
+   /*if(!eed->exe) return ECORE_CALLBACK_CANCEL;*/
 
-   if(eed->size >= (BUFFER_SIZE -1 ))
-     {
-        DBG("Data too big for buffer:%d", eed->size);
-        return ECORE_CALLBACK_DONE;
-     }
+   /*if(eed->size >= (BUFFER_SIZE -1 ))*/
+     /*{*/
+        /*DBG("Data too big for buffer:%d", eed->size);*/
+        /*return ECORE_CALLBACK_DONE;*/
+     /*}*/
 
-   arr = eina_str_split((const char *)eed->data, "[n]", 0);
+   /*arr = eina_str_split((const char *)eed->data, "[n]", 0);*/
    
-   for (i = 0; arr[i]; i++)
-     {
-        if(eina_str_has_prefix(arr[i], "ECOMORPH_OPTIONS:") == EINA_TRUE)
-          {
-             char **options;
-             options = eina_str_split(arr[i], ":", 2);
+   /*for (i = 0; arr[i]; i++)*/
+     /*{*/
+        /*if(eina_str_has_prefix(arr[i], "ECOMORPH_OPTIONS:") == EINA_TRUE)*/
+          /*{*/
+             /*char **options;*/
+             /*options = eina_str_split(arr[i], ":", 2);*/
              
-             if(options[1])
-                config->cmd_options = strdup(options[1]);
+             /*if(options[1])*/
+                /*config->cmd_options = strdup(options[1]);*/
              
-             E_FREE(options[0]);
-             E_FREE(options);
-             INF("CMD_OPTIONS:%s", config->cmd_options);
-          }
+             /*E_FREE(options[0]);*/
+             /*E_FREE(options);*/
+             /*INF("CMD_OPTIONS:%s", config->cmd_options);*/
+          /*}*/
        
-       if(eina_str_has_prefix(arr[i], "ECOMORPH_PLUGINS:") == EINA_TRUE)
-         {
-            char **plugins;
-            plugins = eina_str_split(arr[i], ":", 2);
+       /*if(eina_str_has_prefix(arr[i], "ECOMORPH_PLUGINS:") == EINA_TRUE)*/
+         /*{*/
+            /*char **plugins;*/
+            /*plugins = eina_str_split(arr[i], ":", 2);*/
 
-            if(plugins[1])
-              config->cmd_plugins = strdup(plugins[1]);
+            /*if(plugins[1])*/
+              /*config->cmd_plugins = strdup(plugins[1]);*/
             
-            E_FREE(plugins[0]);
-            E_FREE(plugins);
-            INF("CMD_PLUGINS:%s", config->cmd_plugins);
-         }
+            /*E_FREE(plugins[0]);*/
+            /*E_FREE(plugins);*/
+            /*INF("CMD_PLUGINS:%s", config->cmd_plugins);*/
+         /*}*/
 
-       if(eina_str_has_prefix(arr[i], "ECOMORPH_PLUGINS_OVERRIDE:") == EINA_TRUE)
-         {
-            char **override;
-            override = eina_str_split(arr[i], ":", 2);
+       /*if(eina_str_has_prefix(arr[i], "ECOMORPH_PLUGINS_OVERRIDE:") == EINA_TRUE)*/
+         /*{*/
+            /*char **override;*/
+            /*override = eina_str_split(arr[i], ":", 2);*/
 
-            if(strncmp(override[1],"yes", 3) == 0)
-               config->cmd_plugins_override = EINA_TRUE;
+            /*if(strncmp(override[1],"yes", 3) == 0)*/
+               /*config->cmd_plugins_override = EINA_TRUE;*/
               
-            E_FREE(override[0]);
-            E_FREE(override);
-            INF("CMD_PLUGINS_OVERRIDE:%d", config->cmd_plugins_override);
-         } 
+            /*E_FREE(override[0]);*/
+            /*E_FREE(override);*/
+            /*INF("CMD_PLUGINS_OVERRIDE:%d", config->cmd_plugins_override);*/
+         /*} */
 
-     }
-   E_FREE(arr[0]);
-   E_FREE(arr);
+     /*}*/
+   /*E_FREE(arr[0]);*/
+   /*E_FREE(arr);*/
 
-   return ECORE_CALLBACK_DONE;
-}
+   /*return ECORE_CALLBACK_DONE;*/
+/*}*/
 
-static Eina_Bool
-_e_mod_event_del_handler_ecomp_sh(void *data, int type, void *event)
-{
-   Ecore_Exe_Event_Del *eed;
-   const char *cmd;
+/*static Eina_Bool*/
+/*_e_mod_event_del_handler_ecomp_sh(void *data, int type, void *event)*/
+/*{*/
+   /*Ecore_Exe_Event_Del *eed;*/
+   /*const char *cmd;*/
 
-   if(!(eed = event)) return ECORE_CALLBACK_CANCEL;
-   if(!eed->exe) return ECORE_CALLBACK_CANCEL;
+   /*if(!(eed = event)) return ECORE_CALLBACK_CANCEL;*/
+   /*if(!eed->exe) return ECORE_CALLBACK_CANCEL;*/
 
-   cmd = ecore_exe_cmd_get(eed->exe);
-   if(!cmd) return ECORE_CALLBACK_CANCEL;
+   /*cmd = ecore_exe_cmd_get(eed->exe);*/
+   /*if(!cmd) return ECORE_CALLBACK_CANCEL;*/
 
    
-   if(strcmp(config->cmd_sh, cmd) == 0)
-     {
-        WRN("CMD_SH:%s", config->cmd_sh);
-        config->cmd_sh_ended = EINA_TRUE;
-        ecore_event_handler_del(config->eeh);
+   /*if(strcmp(config->cmd_sh, cmd) == 0)*/
+     /*{*/
+        /*WRN("CMD_SH:%s", config->cmd_sh);*/
+        /*config->cmd_sh_ended = EINA_TRUE;*/
+        /*ecore_event_handler_del(config->eeh);*/
 
-        e_mod_run_ecomorph();
-     }
+        /*e_mod_run_ecomorph();*/
+     /*}*/
 
-   return ECORE_CALLBACK_DONE;
-}
+   /*return ECORE_CALLBACK_DONE;*/
+/*}*/
 
-void
-e_mod_run_ecomp_sh()
-{
-   char *ecomp_sh[] = 
-     {
-        "/usr/bin/ecomp.sh",
-        "/opt/bin/ecomp.sh",
-        "/usr/local/bin/ecomp.sh", 
-        "/opt/local/bin/ecomp.sh"
-     };
-   int i;
+/*void*/
+/*e_mod_run_ecomp_sh()*/
+/*{*/
+   /*char *ecomp_sh[] = */
+     /*{*/
+        /*"/usr/bin/ecomp.sh",*/
+        /*"/opt/bin/ecomp.sh",*/
+        /*"/usr/local/bin/ecomp.sh", */
+        /*"/opt/local/bin/ecomp.sh"*/
+     /*};*/
+   /*int i;*/
 
-   for (i = 0; i < 3; i++)
-      if(ecore_file_exists(ecomp_sh[i]) == EINA_TRUE)
-         break;
+   /*for (i = 0; i < 3; i++)*/
+      /*if(ecore_file_exists(ecomp_sh[i]) == EINA_TRUE)*/
+         /*break;*/
 
-   config->cmd_sh = eina_stringshare_add(ecomp_sh[i]);
+   /*config->cmd_sh = eina_stringshare_add(ecomp_sh[i]);*/
 
-   config->exe = ecore_exe_pipe_run(ecomp_sh[i],
-                                    ECORE_EXE_PIPE_WRITE |
-                                    ECORE_EXE_PIPE_READ_LINE_BUFFERED |
-                                    ECORE_EXE_PIPE_ERROR_LINE_BUFFERED |
-                                    ECORE_EXE_PIPE_ERROR |
-                                    ECORE_EXE_USE_SH |
-                                    ECORE_EXE_PIPE_READ,
-                                    NULL);
+   /*config->exe = ecore_exe_pipe_run(ecomp_sh[i],*/
+                                    /*ECORE_EXE_PIPE_WRITE |*/
+                                    /*ECORE_EXE_PIPE_READ_LINE_BUFFERED |*/
+                                    /*ECORE_EXE_PIPE_ERROR_LINE_BUFFERED |*/
+                                    /*ECORE_EXE_PIPE_ERROR |*/
+                                    /*ECORE_EXE_USE_SH |*/
+                                    /*ECORE_EXE_PIPE_READ,*/
+                                    /*NULL);*/
 
-   config->eeh = ecore_event_handler_add(ECORE_EXE_EVENT_DATA,
-                                         _e_mod_event_data_handler_ecomp_sh, NULL);
+   /*config->eeh = ecore_event_handler_add(ECORE_EXE_EVENT_DATA,*/
+                                         /*_e_mod_event_data_handler_ecomp_sh, NULL);*/
 
-   config->eeh = ecore_event_handler_add(ECORE_EXE_EVENT_DEL,
-                                         _e_mod_event_del_handler_ecomp_sh, NULL);
-   return;
-}
+   /*config->eeh = ecore_event_handler_add(ECORE_EXE_EVENT_DEL,*/
+                                         /*_e_mod_event_del_handler_ecomp_sh, NULL);*/
+   /*return;*/
+/*}*/
 
 Eina_Bool
 _e_mod_xvisual_get(Ecore_X_Display *dpy, int scrnum)
